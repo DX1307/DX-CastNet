fa0be3d9713bc790324a36f13e937bb022088265
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-05-03 00:55:24 +0700

    Update config.lua

--- DX-CastNet/config.lua
+++ DX-CastNet/config.lua
@@ -1,12 +1,35 @@
-DXHIGH = {}
-DXHIGH.lang = 'th'
-DXHIGH.EnableSkillCheck = true -- ปิดเปิด มินิเกม
-DXHIGH.Itemuse = 'fishing_net' -- ไอเทมกดใช้
-DXHIGH.PercentGive = 50 -- เปอร์เซ็นที่จะได้รับของ Droprate
-DXHIGH.PercentRemove = 50 -- เปอร์เซ็น แห พัง
+Config = {}
+Config.lang = 'th'
+Config.EnableSkillCheck = true -- ปิดเปิด มินิเกม
+Config.Itemuse = 'fishing_net' -- ไอเทมกดใช้
+Config.PercentGive = 50 -- เปอร์เซ็นที่จะได้รับของ Droprate
+Config.PercentRemove = 50 -- เปอร์เซ็น แห พัง
 
 
-DXHIGH.Droprate = { -- [ ไอเทมโบนัส เพิิ่มใน sql ]
+Config.Zonefish = {
+    -- Zone
+    EnableZone = true, -- เปิดปิด โซน
+    range = 50.0, -- ความกว้างของโซน
+    location = {x = 2169.87, y =  3850.86, z = 30.38}, -- จุดตรงกลางโซน
+
+    -- Blips
+    EnableBlips		= true, -- เปิดปิด Blips
+    Blips_id		= 1, -- รูปของ Blips
+	Blips_Scale		= 0.6, -- ขนาดของ Blips
+	Blips_Colour	= 2, -- สีของ Blips
+    Blips_Alpha     = 150, -- Alphaของ Blips
+	Blips_Name		= 'Zone Fishing', -- ชื่อของ Blips
+
+    -- Marker
+    EnableMarker = true, -- เปิดปิด Marker
+    Marker_R    = 40, -- สีของ Marker
+    Marker_G    = 98, -- สีของ Marker
+    Marker_B    = 129, -- สีของ Marker
+}
+
+
+
+Config.Droprate = { -- [ ไอเทมโบนัส เพิิ่มใน sql ]
     {
         ItemName = "yang",    -- ไอเทมโบนัส
         ItemCount = {1, 2},    -- จำนวน
@@ -19,7 +42,7 @@ DXHIGH.Droprate = { -- [ ไอเทมโบนัส เพิิ่มใน
 	},
     {
         ItemName = "catfish",    -- ไอเทมโบนัส
-        ItemCount = {1, 2},    -- จำนวน
+        ItemCount = {1, 5},    -- จำนวน
         Percent = 50          -- %
 	},
 }
