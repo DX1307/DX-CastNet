3d38d7005c4cecbfd5e51d7429b45d822de6670c
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-05-03 22:33:09 +0700

    Update config.lua

--- DX-CastNet/config.lua
+++ DX-CastNet/config.lua
@@ -5,6 +5,12 @@ Config.Itemuse = 'fishing_net' -- ไอเทมกดใช้
 Config.PercentGive = 50 -- เปอร์เซ็นที่จะได้รับของ Droprate
 Config.PercentRemove = 50 -- เปอร์เซ็น แห พัง
 
+Config.MiniGame = { -- easy = ช้า // medium = เร็ว // hard = เร็วมากกก
+    Mini_one = 'easy',
+    Mini_two = 'medium',
+    Mini_Three = 'hard',
+    MimiSkillCheck = {'w', 'a', 's', 'd'}, -- RanDom Key
+}
 
 Config.Zonefish = {
     -- Zone
