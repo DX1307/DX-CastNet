e75dc7fe4b8204f19785adaa9a984e5f758db8b5
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-05-15 02:47:20 +0700

    Update client.lua

--- DX-CastNet/Code/client.lua
+++ DX-CastNet/Code/client.lua
@@ -1,6 +1,6 @@
 ESX = exports['es_extended']:getSharedObject()
 local TIME = 4500
-
+local Animation = false
 
 Citizen.CreateThread(function()
     while true do
@@ -19,64 +19,65 @@ end)
 
 RegisterNetEvent('DX-CastNet:DXHIGH:UseItem')
 AddEventHandler('DX-CastNet:DXHIGH:UseItem', function()
-    if Config.Zonefish.EnableZone then
-        local dst = GetDistanceBetweenCoords(vector3(Config.Zonefish.location.x, Config.Zonefish.location.y, Config.Zonefish.location.z), GetEntityCoords(PlayerPedId()), true)
-        if dst < Config.Zonefish.range then
-            TriggerEvent('DX-CastNet:DXHIGH:Loading')
-        else
-            exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['Youarenotinthezone'], TIME, 'error')
-        end
+    local dst = GetDistanceBetweenCoords(vector3(Config.Zonefish.location.x, Config.Zonefish.location.y, Config.Zonefish.location.z), GetEntityCoords(PlayerPedId()), true)
+    if dst < Config.Zonefish.range then
+        TriggerEvent('DX-CastNet:DXHIGH:Loading')
+        exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['UseItemTrue'], TIME, 'error')
     else
-        if IsEntityInWater(GetPlayerPed(-1)) then
-            TriggerEvent('DX-CastNet:DXHIGH:Loading')
-        else
-            exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['Youmustbeinwakewater'], TIME, 'error')
-        end
+        exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['Youarenotinthezone'], TIME, 'error')
     end
 end)
 
 
 RegisterNetEvent('DX-CastNet:DXHIGH:Loading')
 AddEventHandler('DX-CastNet:DXHIGH:Loading', function()
-    local RandomGive = math.random(1,100)
-    Citizen.Wait(800)
-        Anim_1()
-    Citizen.Wait(800)
-        Anim_2()
-    Citizen.Wait(800)
-        Anim_3()
-    Citizen.Wait(2500)
-    if Config.EnableSkillCheck then
-        local success = lib.skillCheck({Config.MiniGame.Mini_one, Config.MiniGame.Mini_two, {areaSize = 60, speedMultiplier = 2}, Config.MiniGame.Mini_Three}, Config.MiniGame.MimiSkillCheck)
-        if success then 
-            if RandomGive > Config.PercentGive then
-                TriggerServerEvent('DX-CastNet:DXHIGH:AddItem')
-                Citizen.Wait(800)
-                TriggerServerEvent('DX-CastNet:DXHIGH:RemoveItem')
-                CancelFishing()
-            else
-                exports['okokNotify']:Alert(Lang[Config.lang]['title'],Lang[Config.lang]['gotnothing'], TIME, 'error')
-                CancelFishing()
-            end
-        else
-            exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['doit'], 5000, 'error')
-            CancelFishing()
-        end
-    else
-        if RandomGive > Config.PercentGive then
-            TriggerServerEvent('DX-CastNet:DXHIGH:AddItem')
+    if not Animation  then
+        Animation = true
+        Citizen.SetTimeout(Config.TimeTouse*1000, function()
+            local RandomGive = math.random(1,100)
             Citizen.Wait(800)
-            TriggerServerEvent('DX-CastNet:DXHIGH:RemoveItem')
-            CancelFishing()
-        else
-            exports['okokNotify']:Alert(Lang[Config.lang]['title'],Lang[Config.lang]['gotnothing'], TIME, 'error')
-            CancelFishing()
-        end
+                Anim_1()
+            Citizen.Wait(800)
+                Anim_2()
+            Citizen.Wait(800)
+                Anim_3()
+            Citizen.Wait(2500)
+            if Config.EnableSkillCheck then
+                local success = lib.skillCheck({Config.MiniGame.Mini_one, Config.MiniGame.Mini_two, {areaSize = 60, speedMultiplier = 2}, Config.MiniGame.Mini_Three}, Config.MiniGame.MimiSkillCheck)
+                if success then 
+                    if RandomGive > Config.PercentGive then
+                        TriggerServerEvent('DX-CastNet:DXHIGH:AddItem')
+                        Citizen.Wait(800)
+                        TriggerServerEvent('DX-CastNet:DXHIGH:RemoveItem')
+                        CancelFishing()
+                    else
+                        exports['okokNotify']:Alert(Lang[Config.lang]['title'],Lang[Config.lang]['gotnothing'], TIME, 'error')
+                        CancelFishing()
+                    end
+                else
+                    exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['doit'], 5000, 'error')
+                    CancelFishing()
+                end
+            else
+                if RandomGive > Config.PercentGive then
+                    TriggerServerEvent('DX-CastNet:DXHIGH:AddItem')
+                    Citizen.Wait(800)
+                    TriggerServerEvent('DX-CastNet:DXHIGH:RemoveItem')
+                    CancelFishing()
+                else
+                    exports['okokNotify']:Alert(Lang[Config.lang]['title'],Lang[Config.lang]['gotnothing'], TIME, 'error')
+                    CancelFishing()
+                end
+            end
+        end)
+    else
+        exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['WaitUseItem'], TIME, 'error')
     end
 end)
 
 
 function Anim_1()
+    Animation = true
     Citizen.Wait(500)
     ESX.Streaming.RequestAnimDict("anim@arena@celeb@flat@solo@no_props@", function()
         TaskPlayAnim(PlayerPedId(), "anim@arena@celeb@flat@solo@no_props@", "slugger_a_player_a", 8.0, -8, -1, 49, 0, 0, 0, 0)
@@ -89,6 +90,7 @@ function Anim_1()
 end
 
 function Anim_2()
+    Animation = true
     exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['dragging'], TIME, 'info')
     Citizen.Wait(500)
     ESX.Streaming.RequestAnimDict("random@mugging4", function()
@@ -101,6 +103,7 @@ function Anim_2()
 end
 
 function Anim_3()
+    Animation = true
     Citizen.Wait(500)
     exports['okokNotify']:Alert(Lang[Config.lang]['title'], Lang[Config.lang]['test'], TIME, 'info')
     ESX.Streaming.RequestAnimDict("move_m@_idles@shake_off", function()
@@ -115,6 +118,7 @@ end
 
 function CancelFishing()
     Citizen.Wait(250)
+    Animation = false
     FreezeEntityPosition(PlayerPedId(), false)
     ClearPedTasksImmediately(GetPlayerPed(-1))
 end
